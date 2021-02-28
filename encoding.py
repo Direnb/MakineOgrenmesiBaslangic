4e0acb8547727bef935038a109447d0980e085c4
Diren <44303178+Direnb@users.noreply.github.com>
2021-02-28 11:31:42 +0300

    Add files via upload

---
+++ encoding.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+
+#veri yukleme
+veriler = pd.read_csv('veriler.csv')
+
+#print(veriler)
+
+ulke = veriler.iloc[:,0:1].values
+#print(ulke)
+
+from sklearn import preprocessing
+
+le = preprocessing.LabelEncoder()
+ulke[:,0] =le.fit_transform(veriler.iloc[:,0])
+
+print(ulke)
+
+ohe = preprocessing.OneHotEncoder()
+ulke = ohe.fit_transform(ulke).toarray()
+print(ulke)
